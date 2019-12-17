8852ef2c91bca1c9f3845665d8037aab8b6657b8
Nikhil Suri <nikhilsuri@comcast.net>
2019-12-17 15:48:41 -0800

    Initial Commit

---
+++ GoalsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoalsApp
+//
+//  Created by Nikhil Suri on 12/17/19.
+//  Copyright © 2019 Nikhil Suri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
